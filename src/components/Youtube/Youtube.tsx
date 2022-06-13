1786ed44bfacc544318d53542a9aca86a4165e9c
avsssai <avsssai@gmail.com>
2022-06-13 21:58:17 +0530

    fixed youtube loader component

--- src/components/Youtube/Youtube.tsx
+++ src/components/Youtube/Youtube.tsx
@@ -1,13 +1,30 @@
+import styled from 'styled-components';
 import MaxWidthWrapper from '../MaxWidthWrapper';
+
+const Container = styled(MaxWidthWrapper)`
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+  max-width: 100%;
+`;
+const Iframe = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+`;
 
 export default function YouTube({ id }: { id: string }) {
   return (
-    <MaxWidthWrapper>
-      <iframe
+    <Container>
+      <Iframe
         src={`https://www.youtube.com/embed/${id}`}
         allow="autoplay; encrypted-media"
         title="Embedded YouTube video"
       />
-    </MaxWidthWrapper>
+    </Container>
   );
 }
